41406b4e101e05346965b349bd878f68f606730c
DESKTOP-OEDPGKJ\wntjd <wntjd7892@naver.com>
2025-02-05 19:09:25 +0900

    25.02.05

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -12,15 +12,35 @@ const LOG_EVENT_MONSTER_ATTACK = 'MONSTER_ATTACK';
 const LOG_EVENT_PLAYER_HEAL = 'PLAYER_HEAL';
 const LOG_EVENT_GAME_OVER = 'GAME_OVER';
 
-const enterValue = prompt('Maximum life for you and the monster.', '100');
 
-let chosenMaxLife = parseInt(enterValue);
 let battleLog = [];
 let lastLoggedEntry;
 
-if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
-    chosenMaxLife = 100;
+function getMaxLifeValues() {
+    const enterValue = prompt('Maximum life for you and the monster.', '100');
+
+    // let chosenMaxLife = parseInt(enterValue);
+    const parsedValue = parseInt(enterValue);
+    if (isNaN(parsedValue) || parsedValue <= 0) {
+        // chosenMaxLife = 100;
+        throw {message: 'Invalid user input, not a number'};
+    }
+    return parsedValue;
 }
+
+let chosenMaxLife;
+
+try {
+    chosenMaxLife = getMaxLifeValues();
+} catch (error) {
+    console.log(error);
+    chosenMaxLife = 100;
+    alert('You entered something wrong, default value of 100 was used.')
+    // throw error;
+} finally {
+    
+}
+
 
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
