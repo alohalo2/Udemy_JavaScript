ad28dd39aede12ec5643a653c6038a7787456516
DESKTOP-OEDPGKJ\wntjd <wntjd7892@naver.com>
2025-02-18 18:21:35 +0900

    25.02.18

---
+++ dom-01-starting-code/app.js
@@ -0,0 +1,36 @@
+// 객체일 때
+const obj = {
+    name: "Alice",
+    age: 25,
+    greet: function() {
+        console.log("Hello!");
+    }
+};
+
+console.log(obj);
+console.dir(obj);
+
+// HTML일 때
+const element = document.querySelector("body");
+
+console.log(element);  // HTML 구조가 출력됨
+console.dir(element);  // 객체의 속성과 메서드가 출력됨
+
+
+const h1 = document.getElementById('main-title');
+
+h1.textContent = 'Some new title';
+h1.style.color = 'white';
+h1.style.backgroundColor = 'black';
+
+const li = document.querySelector('li:last-of-type');
+li.textContent = li.textContent + ' (Changed!)';
+
+const body = document.body;
+
+// const listItemElements = document.querySelectorAll('li');
+const listItemElements = document.getElementsByTagName('li');
+
+for(const listItemEl of listItemElements) {
+    console.dir(listItemEl);
+}
